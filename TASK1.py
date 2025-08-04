fe5bfeac2a1f68bbbee00cca01df3ea1d5af6867
MA7MEDSAMIR <mestrotubegaming@gmail.com>
2025-08-04 10:42:55 +0300

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,9 @@
+tem=25
+if tem>=30:
+    print("It's a hot day. Stay hydrated!")
+if tem>20 and tem<29:
+    print("It's a warm day. Enjoy the weather!")
+if tem>10 and tem<19:
+    print("It's a cool day. Wear a jacket!")
+if tem<10:
+    print("It's a cold day. Stay warm!")
